793723c9b0efb1e2b2b79c29197a521ca0098a00
Theophilus <aimthesun@yahoo.com>
2013-03-27 18:38:22 -0400

    Added code

---
+++ src/SE_spring2013_g8/hal/Main/MainActivity.java
@@ -0,0 +1,23 @@
+package SE_spring2013_g8.hal.Main;
+
+import ece.rutgers.edu.se_spring2013.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
