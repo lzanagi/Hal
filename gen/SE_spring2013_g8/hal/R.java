5f3b741f3aaf6c1e6c801fbaccbaa7c4323ea53a
Theophilus <aimthesun@yahoo.com>
2013-03-29 10:47:22 -0400

    added broadcast, p2pcall, and conference call GUI

--- gen/SE_spring2013_g8/hal/R.java
+++ gen/SE_spring2013_g8/hal/R.java
@@ -28,34 +28,52 @@ public final class R {
         public static final int surveillanceicon=0x7f020004;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080005;
-        public static final int broadcast=0x7f080002;
-        public static final int conf=0x7f080003;
-        public static final int gridview=0x7f080001;
-        public static final int p2p=0x7f080004;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int action_settings=0x7f09000a;
+        public static final int broadcast=0x7f090007;
+        public static final int button1=0x7f090005;
+        public static final int button2=0x7f090006;
+        public static final int chronometer1=0x7f090002;
+        public static final int conf=0x7f090008;
+        public static final int gridview=0x7f090001;
+        public static final int p2p=0x7f090009;
+        public static final int start_button=0x7f090003;
+        public static final int stop_button=0x7f090004;
+    }
+    public static final class integer {
+        public static final int four_rows=0x7f050000;
     }
     public static final class layout {
         public static final int activity_light_control=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int intercom_homeview=0x7f030002;
+        public static final int intercom_broadcast=0x7f030002;
+        public static final int intercom_conference=0x7f030003;
+        public static final int intercom_homeview=0x7f030004;
+        public static final int intercom_p2p=0x7f030005;
     }
     public static final class menu {
-        public static final int light_control=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int light_control=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int Conf=0x7f05000a;
-        public static final int Light_control_title=0x7f050004;
-        public static final int Light_off=0x7f050006;
-        public static final int Light_on=0x7f050005;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int broadcast=0x7f050008;
-        public static final int hello_world=0x7f050002;
-        public static final int p2p=0x7f050009;
-        public static final int title_activity_comms_thread=0x7f050007;
-        public static final int title_activity_light_control=0x7f050003;
+        public static final int Light_control_title=0x7f060004;
+        public static final int Light_off=0x7f060006;
+        public static final int Light_on=0x7f060005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int intercom_broadcast_chronometer=0x7f06000b;
+        public static final int intercom_broadcast_start=0x7f06000c;
+        public static final int intercom_broadcast_stop=0x7f06000d;
+        public static final int intercom_conference_call=0x7f060010;
+        public static final int intercom_conference_cancel=0x7f060011;
+        public static final int intercom_home_Conf=0x7f06000a;
+        public static final int intercom_home_broadcast=0x7f060008;
+        public static final int intercom_home_p2p=0x7f060009;
+        public static final int intercom_p2p_call=0x7f06000e;
+        public static final int intercom_p2p_cancel=0x7f06000f;
+        public static final int title_activity_comms_thread=0x7f060007;
+        public static final int title_activity_light_control=0x7f060003;
     }
     public static final class style {
         /** 
@@ -79,10 +97,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/SE_spring2013_g8/hal/Intercom/HomeView.java
+++ src/SE_spring2013_g8/hal/Intercom/HomeView.java
@@ -2,7 +2,9 @@ package SE_spring2013_g8.hal.Intercom;
 
 import SE_spring2013_g8.hal.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeView extends Activity {
 
@@ -10,7 +12,27 @@ public class HomeView extends Activity {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.intercom_homeview);
 	
+	}
 	
 	
+	public void gotoBroadcastView(View view) {
+	   
+		Intent intent = new Intent(this, BroadcastView.class);
+	    
+	    startActivity(intent);
+	}
+	
+	
+	public void gotoP2PView(View view) {
+		Intent intent = new Intent(this, P2PView.class);
+	    
+	    startActivity(intent);
+	}
+	
+	
+	public void gotoConferenceView(View view) {
+		Intent intent = new Intent(this, ConferenceView.class);
+	    
+	    startActivity(intent);
 	}
 }
